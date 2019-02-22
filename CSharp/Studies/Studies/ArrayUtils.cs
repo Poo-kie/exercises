e409a9911696cf60e18e92cee03cf95ef243105d
pookie <pookie@pookies-MacBook-Pro.local>
2019-02-21 19:40:59 -0500

    adding initial project with exercise

---
+++ CSharp/Studies/Studies/ArrayUtils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Studies
+{
+    public static class ArrayUtils
+    {
+        /// <summary>
+        /// Assumes <paramref name="arr"/> is an array of sorted ints.
+        /// Can be empty or a single element
+        /// </summary>
+        /// <returns>The length of the resized array</returns>
+        /// <param name="arr">an array of sorted ints.</param>
+        public static int RemoveDuplicatesFromSortedInts(ref int[] arr)
+        {
+            var shiftCount = 0;
+            var shiftedArrLength = arr.Length;
+
+            for(var i = 1; i < shiftedArrLength; i++)
+            {
+                if (arr[i] == arr[i-1])
+                {
+                    i--;
+                    shiftCount++;
+                    shiftedArrLength--;
+                    Array.Copy(arr, i + 1, arr, i, arr.Length - (i + 1));
+                }
+            }
+
+            Array.Resize(ref arr, shiftedArrLength);
+
+            return arr.Length;
+        }
+    }
+}
