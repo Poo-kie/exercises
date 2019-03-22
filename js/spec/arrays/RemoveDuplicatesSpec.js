596a9c6de467e059377e0cadd596e090b1313e31
pookie <pookie@pookies-MacBook-Pro.local>
2019-03-22 15:31:06 -0400

    added recursive version of remove dupes and tests

--- js/spec/arrays/RemoveDuplicatesSpec.js
+++ js/spec/arrays/RemoveDuplicatesSpec.js
@@ -26,4 +26,30 @@ describe("RemoveDuplicates", () => {
             expect(length).toEqual(1);
         });
     });
+
+    describe("removeDuplicatesFromSortedInts2", () => {
+        it("should remove duplicates from given array", () => {
+            var arr = [1,2,2,2,2,2,3,3,4]
+            RemoveDuplicates.removeDuplicatesFromSortedInts2(arr);
+            expect(arr).toEqual([1,2,3,4]);
+        });
+    
+        it("should return new length of array", () => {
+            var arr = [1,2,2,2,2,2,3,3,4]
+            let length = RemoveDuplicates.removeDuplicatesFromSortedInts2(arr);
+            expect(length).toEqual(4);
+        });
+    
+        it("should handle empty array", () => {
+            var arr = []
+            let length = RemoveDuplicates.removeDuplicatesFromSortedInts2(arr);
+            expect(length).toEqual(0);
+        });
+    
+        it("should handle single element array", () => {
+            var arr = [100]
+            let length = RemoveDuplicates.removeDuplicatesFromSortedInts2(arr);
+            expect(length).toEqual(1);
+        });
+    });
 });
