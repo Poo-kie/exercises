a9d88dd604fc1d392010c4ddcecbef67f7a96241
pookie <pookie@pookies-MacBook-Pro.local>
2019-03-22 14:42:13 -0400

    adding recursive version of remove duplicates and tests

--- CSharp/Studies/Studies.Tests/Arrays/RemoveDuplicatesTests.cs
+++ CSharp/Studies/Studies.Tests/Arrays/RemoveDuplicatesTests.cs
@@ -57,5 +57,60 @@ namespace Studies.Tests.Arrays
             Assert.Empty(arr);
             Assert.Equal(0, length);
         }
+
+        // 2
+
+        [Fact]
+        public void RemoveDuplicatesFromSortedInts2_ShouldRemoveDuplicatesAndReturnNewArrLength()
+        {
+            var arr = new[] { 0, 1, 2, 2, 2, 2, 5, 5, 5, 100 };
+            var length = RemoveDuplicatesExercises.RemoveDuplicatesFromSortedInts2(ref arr);
+
+            Assert.Equal(5, arr.Length);
+            Assert.Equal(5, length);
+
+            Assert.Equal(0, arr[0]);
+            Assert.Equal(1, arr[1]);
+            Assert.Equal(2, arr[2]);
+            Assert.Equal(5, arr[3]);
+            Assert.Equal(100, arr[4]);
+        }
+
+        [Fact]
+        public void RemoveDuplicatesFromSortedInts2_ShouldNotRemoveNonDuplicates()
+        {
+            var arr = new[] { 0, 1, 2, 3 };
+            var length = RemoveDuplicatesExercises.RemoveDuplicatesFromSortedInts2(ref arr);
+
+            Assert.Equal(4, arr.Length);
+            Assert.Equal(4, length);
+
+            Assert.Equal(0, arr[0]);
+            Assert.Equal(1, arr[1]);
+            Assert.Equal(2, arr[2]);
+            Assert.Equal(3, arr[3]);
+        }
+
+        [Fact]
+        public void RemoveDuplicatesFromSortedInts2_ShouldReturnSingleArrayElement()
+        {
+            var arr = new[] { 0 };
+            var length = RemoveDuplicatesExercises.RemoveDuplicatesFromSortedInts2(ref arr);
+
+            Assert.Single(arr);
+            Assert.Equal(1, length);
+
+            Assert.Equal(0, arr[0]);
+        }
+
+        [Fact]
+        public void RemoveDuplicatesFromSortedInts2_ShouldHandleEmptyArrayWithoutErrors()
+        {
+            var arr = new int[0];
+            var length = RemoveDuplicatesExercises.RemoveDuplicatesFromSortedInts2(ref arr);
+
+            Assert.Empty(arr);
+            Assert.Equal(0, length);
+        }
     }
 }
--- CSharp/Studies/Studies/Arrays/RemoveDuplicatesExercises.cs
+++ CSharp/Studies/Studies/Arrays/RemoveDuplicatesExercises.cs
@@ -29,5 +29,35 @@ namespace Studies.Arrays
 
             return arr.Length;
         }
+
+        /// <summary>
+        /// Recursive version
+        /// https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/727/
+        /// </summary>
+        /// <returns>The length of the resized array</returns>
+        /// <param name="arr">an array of sorted ints.</param>
+        public static int RemoveDuplicatesFromSortedInts2(ref int[] arr)
+        {
+            var newLength = RemoveDuplicatesFromSortedIntsRecursion(ref arr, 1, 0, arr.Length);
+
+            if (arr.Length != newLength) Array.Resize(ref arr, newLength);
+
+            return arr.Length;
+        }
+            
+        private static int RemoveDuplicatesFromSortedIntsRecursion(ref int[] arr, int idx, int shiftCount, int shiftedArrLength)
+        {
+            if (idx >= shiftedArrLength) return shiftedArrLength;
+
+            if (arr[idx] == arr[idx-1])
+            {
+                idx--;
+                shiftCount++;
+                shiftedArrLength--;
+                Array.Copy(arr, idx + 1, arr, idx, arr.Length - (idx + 1));
+            }
+
+            return RemoveDuplicatesFromSortedIntsRecursion(ref arr, ++idx, shiftCount, shiftedArrLength);
+        }
     }   
 }
