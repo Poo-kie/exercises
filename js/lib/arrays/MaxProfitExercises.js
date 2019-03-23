e1ed7c3d25bc183a722c09fd9dd827fd73fa7d0e
pookie <pookie@pookies-MacBook-Pro.local>
2019-03-23 10:50:37 -0400

    adding js recursive maxProfit and tests

--- js/lib/arrays/MaxProfitExercises.js
+++ js/lib/arrays/MaxProfitExercises.js
@@ -32,6 +32,49 @@ class MaxProfitExercises {
 
         return maxProfit;
     }
+
+    /**
+     * 
+     * @param {Array} prices - Prices of various stocks indexed by day
+     * @summary https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/564/
+     * 
+     * @returns The max profit derived from buying and selling stock from {prices}
+     */
+    static maxProfit2(prices) {
+        return this.maxProfitRecursion(prices, 0, 0);
+    }
+
+    static maxProfitRecursion(prices, idx, maxProfit) {
+        let purchase = this.findPurchaseIndex(prices, idx);
+
+        if (!purchase.found) return maxProfit;
+
+        let sell = this.findSellIndex(prices, idx + 1);
+        maxProfit += prices[sell.index] - prices[purchase.index];
+
+        return this.maxProfitRecursion(prices, sell.index + 1, maxProfit);
+    }
+
+    static findPurchaseIndex(prices, idx) {
+        if (idx >= (prices.length - 1)) return new FindResult(false, -1);
+
+        if ((prices[idx + 1] - prices[idx]) > 0) return new FindResult(true, idx);
+
+        return this.findPurchaseIndex(prices, ++idx);
+    }
+
+    static findSellIndex(prices, idx) {
+        if (idx >= (prices.length - 1) || (prices[idx] - prices[idx + 1]) > 0) return new FindResult(true, idx);
+
+        return this.findSellIndex(prices, ++idx);
+    }
+}
+
+class FindResult {
+    constructor(found, idx) {
+        this.found = found;
+        this.index = idx;
+    }       
 }
 
 module.exports = MaxProfitExercises;
